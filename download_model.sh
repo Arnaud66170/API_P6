aa1204e193d8948ec094a26c7c15e702f28c27ac
nounouille <arnauddev66@gmail.com>
2025-02-23 14:03:27 +0100

    2

---
+++ download_model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Définition du dossier cible
+MODEL_DIR="models"
+MODEL_NAME="InceptionV3.h5"
+FILE_ID="1_17-7b2xlYOKyEWYIg1lDfZqN2lvi5CA"  # ID du fichier Google Drive
+
+# Vérifier si le dossier existe, sinon le créer
+mkdir -p $MODEL_DIR
+
+# Télécharger le fichier depuis Google Drive
+echo "Téléchargement de $MODEL_NAME depuis Google Drive..."
+curl -L -o $MODEL_DIR/$MODEL_NAME "https://drive.google.com/uc?export=download&id=$FILE_ID"
+
+echo "Le fichier a été téléchargé dans $MODEL_DIR/$MODEL_NAME"
